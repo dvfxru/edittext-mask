c56263d9f40bd61327ce711b83f5a62403206411
DiXL0f0S <vladislav-psarov@yandex.ru>
2021-03-24 14:04:25 +1000

    Deleted "Settings" option menu item in example app

--- app/src/main/java/ru/egslava/edittextphonenumber/MainActivity.java
+++ app/src/main/java/ru/egslava/edittextphonenumber/MainActivity.java
@@ -2,9 +2,6 @@ package ru.egslava.edittextphonenumber;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 import br.com.sapereaude.maskededittext.MaskedEditText;
 
@@ -14,21 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MaskedEditText phone = findViewById(R.id.phone_input);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        MaskedEditText maskedEditText = findViewById(R.id.phone_input);
     }
 }
